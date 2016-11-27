f2664f2f81882c7b5d1534b5a134668312b065d3
Brian Dorton <dorton@gmail.com>
2016-11-26 21:29:25 -0600

    adds home screen and devise to users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @season = Season.where('seasons.year <= ?', Date.today.end_of_year).first
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   attachment :profile_image
   has_many :user_seasons
   has_many :seasons, through: :user_seasons
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
