7f6468f9cf31cce8e8527ecfdcda9b092fcd0af2
Brian Dorton <dorton@gmail.com>
2016-12-05 15:16:34 -0600

    readies production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { host: 'secretsantapp.heroku.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
